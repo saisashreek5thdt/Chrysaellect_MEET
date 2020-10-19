08b25465d21cbab3a330f6a241d5fff45a5c3697
ranadheer <ranadheer@5thdt.com>
2020-10-19 18:53:15 +0530

    Commit

--- assets/js/auto_complete.js
+++ assets/js/auto_complete.js
@@ -100,3 +100,4 @@ var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
 
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), countries);
+autocomplete(document.getElementById("myCourses"), countries);
